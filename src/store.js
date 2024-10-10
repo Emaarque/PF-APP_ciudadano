f75c6cf0f9a163327fc89926aa8ba57a8f2fdeb5
Emaarque <ciberema@example.com>
2024-10-10 12:01:50 -0300

    feat: agregar los middleware de auth y reclamo

--- src/store.js
+++ src/store.js
@@ -2,16 +2,20 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { authApi } from "./services/authApi";
+import { reclamoApi } from './services/reclamoApi';
 import authReducer from "./features/auth/authSlice";
+import reclamoReducer from './features/reclamo/reclamoSlice';
 
 export const store = configureStore({
   reducer: {
     [authApi.reducerPath]: authApi.reducer,
+    [reclamoApi.reducerPath]: reclamoApi.reducer,
     auth: authReducer,
+    reclamo: reclamoReducer,
     // Otros reductores aquí
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(authApi.middleware),
+    getDefaultMiddleware().concat(authApi.middleware, reclamoApi.middleware),
 });
 
 setupListeners(store.dispatch);
